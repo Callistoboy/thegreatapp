a8fa1a78dbb987074e71fb127fb1f0bf1ea40a56
Callistoboy <pavskij.ai@edu.spbstu.ru>
2020-06-09 22:43:48 +0300

    i18n

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Thegreatapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [:en]
+    config.i18n.enforce_available_locales = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'main#index'
-  get 'main/index'
-  get 'main/help'
-  get 'main/contacts'
-  get 'main/about'
+  scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'main/index'
+    get 'main/help'
+    get 'main/contacts'
+    get 'main/about'
+  end
 end
